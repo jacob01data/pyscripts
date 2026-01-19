88c7aa299478c25ade161d34e3cb46780d90c0c2
luayhen_firstgitacc <testacc22lol@gmail.com>
2026-01-19 18:55:03 +0900

    Improve installer function to handle missing modules
    Enhanced the installer function to attempt module installation if not found.

--- install_module.py
+++ install_module.py
@@ -1,6 +1,22 @@
 # havent used python in a long time so there might be errors
 # ive been using LuaU/Lua alot
 
-def installer(module)
+import importlib # built in
+__import__ = importlib.import_module
 
-# i need to poop ill finish this later
+def installer(module):
+  module_or_none = None
+  try:
+    module_or_none = __import__(module)
+  except Exception as e:
+    # install it with os.system
+    os = __import__("os") # built in btw
+    # check to see if it is invalid module name
+    try:
+      os.system("pip install " + module)
+    except Exception as e:
+      print("Invalid module Name!")
+      return None
+    # once complete set
+    module_or_none = __import__(module)
+  return module_or_none
